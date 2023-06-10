e6b4ec53af32e12e35bf60af66a8d1f4c1121d89
MohsenRazvi <m.razvi@se16.qmul.ac.uk>
2023-06-10 13:02:30 +0100

    before build

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import projectsList from "./projects.json"
 import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
 
 import { ProjectCard } from './components/project-card.component';
@@ -17,40 +18,46 @@ function App() {
   const data=projectsList.projects
   const stacks=projectsList.techs
   const slides=projectsList.slides
-  // const images = ["main","cloud","front","py"]
+
+  const AutoplaySlider =  withAutoplay(AwesomeSlider);
+  
   return (
     <div className="App">
       <div className='App-tag'>
         <div>Mohsen Razvi</div>
         <div style={{display:'flex',fontSize:'15px'}}>
-          <div>Software Engineer | </div>
-          <div> Full Stack Developer | </div>
+          <div>Software Engineer|</div>
+          <div> Full Stack Developer| </div>
           <div>Quant Developer</div>
 
         </div>
-        <button style={{backgroundColor:'#2594fd',fontFamily:'inherit',height:'50px',width:'150px',border:'2px solid white',borderRadius:'5px 5px',color:'white'}} onClick={() => window.location.href = 'mailto:mohsenrazvi@gmail.com'}>Contact Me</button>
-
+        <button className='Card-btn'  onClick={() => window.location.href = 'mailto:mohsenrazvi@gmail.com'}>Contact Me</button>
+        
       </div>
       <Carousel stacks={stacks}/>
 
       <header className="App-header">
-        <AwesomeSlider style={{height:'50vh'}}>
-          
-          <div style={{backgroundImage:`url(${bg0})`,backgroundSize:'cover',backgroundPosition:'0% 10%'}}>
-            <Slide props={slides[0]}/>
-          </div>
+      <AutoplaySlider
+        play={true}
+        cancelOnInteraction={false}
+        interval={6000}
+        style={{height:'50vh'}}
+      >
           <div style={{backgroundImage:`url(${bg3})`,backgroundSize:'cover',backgroundPosition:'0% 40%'}}>
-            <Slide props={slides[1]}/>
+            <Slide {...slides[0]}/>
+          </div>
+          <div style={{backgroundImage:`url(${bg0})`,backgroundSize:'cover',backgroundPosition:'0% 10%'}}>
+            <Slide {...slides[1]}/>
           </div>
           <div style={{backgroundImage:`url(${bg2})`,backgroundSize:'cover',backgroundPosition:'0% 50%'}}>
-            <Slide props={slides[2]}/>
+            <Slide {...slides[2]}/>
           </div>
           <div style={{backgroundImage:`url(${bg1})`,backgroundSize:'cover',backgroundPosition:'0% 30%'}}>
-            <Slide props={slides[3]}/>
+            <Slide {...slides[3]}/>
           </div>
-          {/* <div style={{backgroundImage:`url(${bg1})`,backgroundSize:'cover'}}>API development</div> */}
-          
-        </AwesomeSlider>
+
+      </AutoplaySlider>
+        
         <div className='Section-title'>Projects</div>
         
         <div className='Card-container'>
@@ -59,19 +66,19 @@ function App() {
             ))}
           
         </div>
-        <div  style={{width:'100%', height:'30vh'}}>More projects coming soon...</div>
+        <div className='Section-title'style={{margin:'0% 0% 3% 10%'}} >More projects coming soon...</div>
       </header>
       <footer className="App-footer">
-        <div >
-          Built with React.js
+        <div style={{display:'flex',alignItems:'center'}}>
+          <code>Built with React.js</code>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <div>
-          Work with me
-          <button>Send me an email</button>
+        <div style={{display:'flex',alignItems:'center'}}>
+          {/* style={{backgroundColor:'#2594fd',fontFamily:'inherit',height:'50px',width:'150px',border:'2px solid white',borderRadius:'5px 5px',color:'white'}} */}
+        <button className='Card-btn' style={{marginLeft:'0'}} onClick={() => window.location.href = 'mailto:mohsenrazvi@gmail.com'}>Contact Me</button>
         </div>
       </footer>
-    </div>
+    </div> 
   );
 }
 
--- src/components/project-card.component.jsx
+++ src/components/project-card.component.jsx
@@ -1,20 +1,34 @@
 import '../App.css';
 
-export const ProjectCard = ({id,title,description,link}) => {
+export const ProjectCard = ({id,title,description,link,techs}) => {
     return(
         <div className="Card-box">
             <div className="Card-title">{title}</div>
-            <div className="Card-desc">{description}</div>
-            <button className="Card-btn" href={link}>
-                <a
+            <div style={{display:'flex',flexFlow:'row wrap',justifyContent:'center',marginBottom:'3%'}}>
+                {techs.map((tech) => (
+                    <div style={{backgroundColor:'rgba(255,255,255,0.3)',margin:'1%',padding:'2px 4px',borderRadius:'7px',fontSize:'16px'}}><code>{tech}</code></div>
+                ))}
+
+            </div>
+            <ul>
+            {description.map((bullet) =>(
+
+                <li className="Card-desc">{bullet}</li>
+            ))}
+
+            </ul>
+            
+            <button className="Card-btn" onClick={() => window.location.href = 'mailto:mohsenrazvi@gmail.com'}>
+                {/* <a
                 className="App-link"
                 href={link}
                 target="_blank"
                 rel="noopener noreferrer"
                 >
                 See this project on GitHub
-                </a>
-                </button>
+                </a> */}
+                Ask me about this project
+            </button>
                 
         </div>
     )
